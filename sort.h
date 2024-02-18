3b9780a36e778dfdf828e68638170a25e679749a
amine-zzr <aminezoukri@gmail.com>
2024-02-18 23:15:38 +0000

    Sorts an array of integers in ascending order using the selection sort algorithm

--- sort.h
+++ sort.h
@@ -3,9 +3,6 @@
 
 #include <stdlib.h>
 
-void print_array(const int *array, size_t size);
-void bubble_sort(int *array, size_t size);
-void swap(int *x, int *y);
 
 /**
  * struct listint_s - Doubly linked list node
@@ -21,5 +18,12 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+
+void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
+void swap(int *x, int *y);
+void insertion_sort_list(listint_t **list);
+void print_list(const listint_t *list);
+void selection_sort(int *array, size_t size);
 
 #endif
