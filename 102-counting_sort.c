59d3252ce743f98a3f54ccb34a96e2eba0ebce74
rakati <ouhaddounouredine@gmail.com>
2024-02-20 19:15:51 +0100

    add counting sort algorithm for sorting an array

---
+++ 102-counting_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * counting_sort - Sorts an array of integers in ascending order
+ * using counting sort
+ * @array: Pointer to the array to be sorted
+ * @size: Number of elements in the array
+ *
+ */
+void counting_sort(int *array, size_t size)
+{
+	int *count, *output;
+	size_t i, j, mx = 0;
+
+	if (array == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		if (mx < (size_t)array[i])
+			mx = array[i];
+	count = malloc(sizeof(int) * (mx + 1));
+	output = malloc(sizeof(int) * size);
+	if (!count || !output)
+		return;
+	for (i = 0; i < mx + 1; i++)
+		count[i] = 0;
+	for (i = 0; i < size; i++)
+	{
+		j = array[i];
+		count[j] = count[j] + 1;
+	}
+	for (i = 1; i <= mx; i++)
+		count[i] = count[i] + count[i - 1];
+	print_array(count, mx + 1);
+	for (i = size - 1; ; i--)
+	{
+		j = array[i];
+		count[j] = count[j] - 1;
+		output[count[j]] = array[i];
+		if (i == 0)
+			break;
+	}
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
+	free(count);
+	free(output);
+}
--- sort.h
+++ sort.h
@@ -29,5 +29,6 @@ void quick_sort(int *array, size_t size);
 int partition(int *array, int start, int end, size_t size);
 void quick_sort_recursive(int *array, int start, int end, size_t size);
 void insertion_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 
 #endif
